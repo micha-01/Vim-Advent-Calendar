7ed7685f1a47e9e25e3aa931e279c97a9c9112dc
Micha <65395420+micha-01@users.noreply.github.com>
2022-12-07 23:31:34 +0100

    Updated README for auto password upload

--- write_password.py
+++ write_password.py
@@ -8,7 +8,7 @@ def get_passwords() -> dict:
     password_file: str = 'passwords.txt'
     with open(ROOT.joinpath(password_file).as_posix()) as f:
         for i in range(24):
-            passwords[i + 1] = f.readline().split(" ")[-1]
+            passwords[i + 1] = f.readline().split(" ")[-1].replace('\n', '')
     return passwords
 
 
@@ -25,9 +25,9 @@ def main():
     passwords: dict = get_passwords()
     today: int = find_day()
     readme: str = 'README.md'
-    passwords: str = 'passwords.md'
+    passwords_file: str = 'passwords.md'
     write_pw_file(today, passwords[today], readme)
-    write_pw_file(today, passwords[today], passwords)
+    write_pw_file(today, passwords[today], passwords_file)
 
 
 if __name__ == "__main__":
