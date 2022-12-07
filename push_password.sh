1a8b664caa048a2688bd29665d6f0e6d3f698ce9
Micha <65395420+micha-01@users.noreply.github.com>
2022-12-07 23:31:35 +0100

    refactored push script

--- push_password.sh
+++ push_password.sh
@@ -2,16 +2,15 @@
 
 # files and directories
 README="README.md"
-REPO_DIR="$HOME/Documents/Programming/Vim-Calendar"
-PW_FILE="$REPO_DIR/passwords.txt"
+REPO_DIR="$HOME/Documents/Programming/Vim-Advent-Calendar"
+PW_FILE="$REPO_DIR/passwords.md"
 
 # current day and path to current door
 printf -v DAY '%(%-d)T' -1 # Save current day as integer (e.g. 12) in DAY 
-DOOR="$REPO_DIR/Door_$DAY"
 
 # stage, commit and push to github
 cd $REPO_DIR
 COMMIT_MSG="Updated $README on day $DAY"
-git add "$REPO_DIR/$README" $DOOR
+git add "$REPO_DIR/$README" $PW_FILE
 git commit -m "$COMMIT_MSG"
 git push -u origin main
